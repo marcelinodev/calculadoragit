5d88571730ee5871ad89342e38b5bbaa1a82bf6b
Marcelino Monzon <marcelinomonzon@gmail.com>
2023-05-19 15:45:30 -0300

    Termine la presentacion

--- calculadora.py
+++ calculadora.py
@@ -1,6 +1,6 @@
 
 
-operador = float(input('Seleccione numero 0, 1, 2, 3, 4, 5, 6, 7, 8, 9'))
+operador = float(input('Seleccione numero 1, 2, 3, 4: '))
 if operador == 1:
     print('sumar')
 elif operador == 2:
@@ -9,8 +9,16 @@ elif operador == 3:
     print('multiplicar')
 elif operador == 4:
     print('dividir')
+else:
+    print('seleccione nuebamente')
 
-def suma(a, b):
-    suma = (num1 + num2):
+num1 = input('Primer numero: ')
+num2 = input('segundo numero: ')
+resultado = operador
+print('Este es el total', resultado)
+
+
+'''def suma(a, b):
+    suma = (num1 + num2)
     return resultado
-print(suma(5, 15))
+print(suma(5, 15))'''
